1ac8ffdc6ba7f99b0dfb937a3d6441aa00ed4779
Riya Horo <riyahoro2@gmail.com>
2025-12-02 02:35:49 +0530

    Spotify api changes2

--- backend/spotify_api.py
+++ backend/spotify_api.py
@@ -1,18 +1,14 @@
 import os
 import spotipy
-from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
 
-# Load .env file
-load_dotenv()
-
-CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
-
-if not CLIENT_ID or not CLIENT_SECRET:
-    raise ValueError("Spotify credentials not found. Check environment variables.")
-
 def get_spotify_client():
+    CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+    CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+    if not CLIENT_ID or not CLIENT_SECRET:
+        raise Exception("Spotify API credentials not found")
+
     auth_manager = SpotifyClientCredentials(
         client_id=CLIENT_ID,
         client_secret=CLIENT_SECRET
