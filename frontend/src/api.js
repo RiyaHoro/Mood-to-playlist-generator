b8b77a985cf4048badda310e9c420f2488baec45
Riya Horo <riyahoro2@gmail.com>
2025-12-02 00:00:59 +0530

    added api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://mood-to-playlist-generator.onrender.com/predict";
 
 export const predictMood = async (text) => {
   const res = await axios.post(`${API}/predict`, { text });
